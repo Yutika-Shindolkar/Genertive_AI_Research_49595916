011af7da1228e6ce3dbf868699ff255da9f15b16
zx5g456 <s4907816@student.uq.edu.au>
2026-04-29 13:00:15 +1000

    for the test

--- STUFF/test.py
+++ STUFF/test.py
@@ -1,1 +1,7 @@
 This is a python script for data cleaning
+import pandas as pd
+import numpy as np
+# Load the dataset
+data = pd.read_csv('dataset.csv')
+# Check for missing values
+print(data.isnull().sum())
